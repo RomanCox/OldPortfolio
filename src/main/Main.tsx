dbe544896e5742809476f4b6842bcfa889ff8022
RomanCox <rom.cox@gmail.com>
2022-06-16 19:15:01 +0300

    Main Page without video and animation

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import style from './Main.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
+import styleContainer from '../common/styles/Container.module.css';
+import hexagon from '../assets/image/hexagons.jpg';
 
 export const Main = () => {
+
+    const hexagonBG = {
+        backgroundImage: `url(${hexagon})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    };
+
     return (
-        <div className={style.mainBlock}>
-            <div className={`${styleContainer.container} ${style.mainContainer}`}>
-                <div className={style.welcome}>
-                    <span>Hi There!</span>
-                    <h1> I am Roman Cox</h1>
-                    <p>Frontend developer</p>
-                </div>
-                <div className={style.photo}>
+        <section className={style.mainBlock} style={hexagonBG}>
+            <div className={style.container}>
+                <div className={style.center}>
+                    {/*<div className={`${styleContainer.container} ${style.mainContainer}`}>*/}
+                    <div className={style.textContainer}>
+                        <h1>Roman Cox</h1>
+                        <h4>I'm a
+                            <span className={style.text}>
+                                <span className={style.hiddenText}>developer</span>
+                                <span className={style.hiddenText}>freelancer</span>
+                            </span>
+                        </h4>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
